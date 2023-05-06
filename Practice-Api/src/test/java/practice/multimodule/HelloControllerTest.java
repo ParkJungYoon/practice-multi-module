6171994630f1bb8a964c4dd1a643f4ee234ac9d2
ParkJungYoon <tbr06057@naver.com>
2023-05-06 20:19:35 +0900

    build: ci test 추가

---
+++ Practice-Api/src/test/java/practice/multimodule/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package practice.multimodule;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class HelloControllerTest {
+    @Test
+    public void HelloControllerTest() throws Exception {
+        //then
+        Assertions.assertEquals(1, 1);
+    }
+
+}
